e8d877dc9902fa9eb7ca2c763d95da70322d4e48
shaikh1994 <shaikh95@live.com>
2021-08-26 23:46:42 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://i.imgur.com/XIT7qCD.png",
   },
   {
     title: "DevChat",
