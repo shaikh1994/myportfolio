7d39493c47f1998170c2543ca642e6bd1184f242
shaikh1994 <shaikh95@live.com>
2021-08-27 00:08:55 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,12 +8,12 @@ export const projects = [
     link: "https://reactbootcamp.com",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Game Website",
+    subtitle: "Javascript",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "JavaScript, BootStrap, HTML, CSS",
+    image: "https://i.imgur.com/sKTc1lI.png",
+    link: "https://goofy-heyrovsky-b23ed8.netlify.app/",
   },
   {
     title: "DevChat",
