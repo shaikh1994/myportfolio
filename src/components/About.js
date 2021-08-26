cbaf4a9fc016b05f534109a269b9777ce97ecfe8
shaikh1994 <shaikh95@live.com>
2021-08-27 02:12:42 +0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,12 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Shaikh.
+            <br className="hidden lg:inline-block" />I love to code.
+           
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            I am a Full-Stack Developer. Currently focusing on building amazing websites along with app developement with flutter and machine learning.
           </p>
           <div className="flex justify-center">
             <a
