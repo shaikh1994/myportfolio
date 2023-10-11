536b5f73bf53eda77662c4cb4d60915831f47b97
shaikh1994 <shaikh95@live.com>
2023-10-12 02:31:48 +0800

    adress and phone number changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -40,6 +40,7 @@ export default function Contact() {
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/search?q=dhaka&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+            // https://www.google.com/maps/embed/v1/search?q=dhaka&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8
           />
           <div className="bg-gray-900 pr-10 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
@@ -47,8 +48,8 @@ export default function Contact() {
                 ADDRESS
               </h2>
               <p className="mt-1">
-              Mohammadpur <br />
-              Dhaka, Bangladesh
+              Gelugor <br />
+              Penang, Malaysia
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-1">
@@ -61,7 +62,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">+8801616566874</p>
+              <p className="leading-relaxed">+60169632044</p>
             </div>
           </div>
         </div>
