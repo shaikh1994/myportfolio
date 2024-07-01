00817744dcfaa3bfd3f3f3d14868a9c96d10b0ab
shaikh1994 <shaikh95@live.com>
2024-07-01 22:18:14 +0600

    a

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,11 +11,6 @@ export default function Projects() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
-          {/* <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
-          </p> */}
         </div>
         <div className="flex flex-wrap -m-5">
           {projects.map((project) => (
@@ -23,16 +18,17 @@ export default function Projects() {
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4"
-              target="_blank"  
-              rel="noopener noreferrer">
-              <div className="flex relative">
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="flex relative border-4 border-gray-800 rounded-lg overflow-hidden">
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  style={{ objectFit: 'cover', height: '100%', aspectRatio: '4 / 3' }} 
+                  style={{ objectFit: 'cover', height: '100%', aspectRatio: '4 / 3' }}
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
